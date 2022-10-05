9f97f1a8cc8e7e9f68fa91d56917fa240fc476ce
tatebonham <tatebonham@gmail.com>
2022-10-05 16:23:42 -0700

    Tates component

---
+++ src/components/students/Tate.jsx
@@ -0,0 +1,8 @@
+export default function Tate(){
+    return (
+        <div>
+            <h1>Tate</h1>
+            <img src='https://pbs.twimg.com/media/FeMJLc5WQAAW1YE?format=png&name=240x240'></img>
+        </div>
+    )
+}
