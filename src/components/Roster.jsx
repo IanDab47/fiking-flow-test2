ada8c9d49820482a186d807cc38c6d050007c79c
yasahaverland <yasahaverland@icloud.com>
2022-10-05 19:32:33 -0400

    Yasmin

--- src/components/Roster.jsx
+++ src/components/Roster.jsx
@@ -1,11 +1,13 @@
 // imports
 import Gabe from './students/Gabe'
+import Yasmin from './students/Yasmin'
 
 export default function Roster() {
     return (
       <>
         <h2>I am a Roster!</h2>
         <Gabe />
+        <Yasmin />
       </>
     )
   }
---
+++ src/components/students/Yasmin.jsx
@@ -0,0 +1,12 @@
+import './Yasmin.css';
+
+export default function Yasmin(){
+    return (
+        <div className='Yasmin'>
+            <h1> Yasmin's page </h1>
+
+            <iframe src="https://giphy.com/embed/JfpBjRpzE7nFV4XcRq" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/MeanGirls-mothers-day-mean-girls-cool-mom-JfpBjRpzE7nFV4XcRq">via GIPHY</a></p>
+
+        </div>
+    )
+}
