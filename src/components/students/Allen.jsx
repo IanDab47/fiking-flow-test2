8955177f75cbc18930e5e9cc0738ebe5f0565422
average-alien <allentang113@gmail.com>
2022-10-05 16:22:03 -0700

    adds my component

---
+++ src/components/students/Allen.jsx
@@ -0,0 +1,8 @@
+export default function Allen() {
+    return (
+        <div>
+            <h3>Allen 👋</h3>
+            <img src="https://httpstatusdogs.com/img/418.jpg" alt="dogs in a teapot" />
+        </div>
+    )
+}
