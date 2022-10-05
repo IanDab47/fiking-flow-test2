10c7673c49c696fd3ec584ef14f0ec181518b260
syorn96 <syorn2014@gmail.com>
2022-10-05 16:47:33 -0700

    updates stephen img

--- src/components/students/Stephen.jsx
+++ src/components/students/Stephen.jsx
@@ -2,6 +2,7 @@ export default function Stephen() {
     return (
         <div>
             <h1>🤓 S t e p h e m | 🧿 | Y o r m 🦂</h1>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/c/ca/Bruce_Lee_1973.jpg'></img>
         </div>
     )
 }
