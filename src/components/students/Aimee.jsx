3aca4b0d956da3fcd0802a26cf42509f9357a1f9
aimeepham14 <aimee.pham14@gmail.com>
2022-10-05 16:11:45 -0700

    Adding Aimee to students

---
+++ src/components/students/Aimee.jsx
@@ -0,0 +1,12 @@
+function Aimee () {
+
+    return(
+        <div>
+        <h1>AIMEE</h1>
+
+        <h3> I am currently living in the bay area</h3>
+
+        <p>Fun fact: I am running a full marathon this Sunday!!!</p>
+        </div>
+    )
+}
