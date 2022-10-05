22eab351894f24a10eb484c1aa3e3365f503251f
syorn96 <syorn2014@gmail.com>
2022-10-05 16:25:14 -0700

    adds n to steven

--- src/components/students/Stephen.jsx
+++ src/components/students/Stephen.jsx
@@ -1,7 +1,7 @@
 export default function Stephen(){
     return(
         <div>
-            <h1>🤓 S t e p h e m | 🧿 | Y o r n 🦂</h1>
+            <h1>🤓 S t e p h e n | 🧿 | Y o r n 🦂</h1>
             <img src='https://upload.wikimedia.org/wikipedia/commons/c/ca/Bruce_Lee_1973.jpg'></img>
         </div>
     )
