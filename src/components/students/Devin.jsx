93735a2e1d4e41a37371cd86b33a5098fac89492
devin-lynch <devin.l.lynch@gmail.com>
2022-10-05 18:26:20 -0500

    create student

---
+++ src/components/students/Devin.jsx
@@ -0,0 +1,9 @@
+export default function Devin() {
+    return (
+        <div>
+            <p>why do scuba divers roll backwards off the boat?</p>
+            <br></br>
+            <p>if they rolled forwards they'd still be in the boat.</p>
+        </div>
+    )
+}
