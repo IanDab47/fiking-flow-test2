7549a1187d71df9a7fa4a8365dda6c65605a8959
joshdewhurst <joshuardewhurst@gmail.com>
2022-10-05 16:21:50 -0700

    adding Josh

---
+++ src/components/students/Josh.jsx
@@ -0,0 +1,8 @@
+export default function Josh () {
+    return (
+        <div>
+            <img src='https://i.postimg.cc/fyt3JkVh/Josh-Photo.jpg' alt="Josh Photo" />
+            <h1>Josh</h1>
+        </div>
+    )
+}
