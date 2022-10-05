362327d7960fb31913094a989ae5f28443fa7616
yasahaverland <yasahaverland@icloud.com>
2022-10-05 19:43:07 -0400

    yasmin

--- src/components/Roster.jsx
+++ src/components/Roster.jsx
@@ -1,13 +1,11 @@
 // imports
 import Gabe from './students/Gabe'
-import Yasmin from './students/Yasmin'
 
 export default function Roster() {
     return (
       <>
         <h2>I am a Roster!</h2>
         <Gabe />
-        <Yasmin />
       </>
     )
   }
